8fd5fa333189266f149c471d2229d5911b2f08e3
will-weiss <will.weiss1230@gmail.com>
2016-07-06 23:48:51 -0400

    update template to compute non-string types depending on context

--- test/unit/templates.test.js
+++ test/unit/templates.test.js
@@ -4,15 +4,9 @@ var test = require('tape');
 var template = require('../../lib/engine_util').template;
 
 // TODO:
-// plain strings
-// string with a {{}}
-// string with multiple {{}}s
 // string with a function
 // string with multiple functions
 // string with a function and a {{}}
-// same but for an object
-
-// variables that aren't defined
 // functions that aren't defined
 
 var emptyContext = { vars: {} };
@@ -23,8 +17,67 @@ test('templating a plain string should return the same string', function(t) {
   t.end();
 });
 
-test.test('variables can be substituted', function(t) {
+test.test('string variables can be substituted', function(t) {
   t.assert(template('hello {{name}}', { vars: { name: 'Hassy'} }) === 'hello Hassy', '');
   t.assert(template('hello {{name}}', emptyContext) === 'hello ', '');
+  t.end();
+});
+
+test.test('strings with multiple variables can be substituted', function(t) {
+  t.assert(template('hello {{nameFirst}} {{nameLast}}', { vars: { nameFirst: 'Neil', nameLast: 'Armstrong'} }) === 'hello Neil Armstrong', '');
+  t.end();
+});
+
+test.test('substituted string variables are not HTML escaped', function(t) {
+  t.equal(template('{{lawFirm}}', { vars: { lawFirm: 'Michelson, Jones & Peterson LLC.'} }), 'Michelson, Jones & Peterson LLC.', '');
+  t.end();
+});
+
+test.test('numeric variables can be substituted', function(t) {
+  t.equal(template('{{int}}', { vars: { int: 5 } }), 5, '');
+  t.end();
+});
+
+test.test('whole objects can be substituted', function(t) {
+  t.deepEqual(template('{{obj}}', { vars: { obj: { nested: 'data' } } }), { nested: 'data' }, '');
+  t.end();
+});
+
+test.test('when concatenated with other strings, null and undefined are substituted as an empty string', function(t) {
+  t.equal(template('hello {{name}}', { vars: { name: null } }), 'hello ', '');
+  t.equal(template('hello {{name}}', { vars: { name: undefined } }), 'hello ', '');
+  t.equal(template('hello {{name}}', { vars: {} }), 'hello ', '');
+  t.end();
+});
+
+test.test('when substituted on their own, null and undefined retain their original values', function(t) {
+  t.equal(template('{{name}}', { vars: { name: null } }), null, '');
+  t.equal(template('{{name}}', { vars: { name: undefined } }), undefined, '');
+  t.equal(template('{{name}}', { vars: {} }), undefined, '');
+  t.end();
+});
+
+test.test('dotted variables can be substituted', function(t) {
+  const context = {
+    vars: {
+      nested: {
+        str: 'someText',
+        int: 5,
+        emptyString: '',
+        explicitNull: null,
+        explicitUndefined: undefined,
+        deeply: {
+          some: 'data'
+        }
+      }
+    }
+  };
+  t.deepEqual(template('{{nested.str}}', context), 'someText', '');
+  t.deepEqual(template('{{nested.int}}', context), 5, '');
+  t.deepEqual(template('{{nested.emptyString}}', context), '', '');
+  t.deepEqual(template('{{nested.explicitNull}}', context), null, '');
+  t.deepEqual(template('{{nested.explicitUndefined}}', context), undefined, '');
+  t.deepEqual(template('{{nested.deeply}}', context), {some: 'data'}, '');
+  t.deepEqual(template('{{nested.implicitUndefined}}', context), undefined, '');
   t.end();
 });
